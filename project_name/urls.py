198cd9514c88aa34ae95ddc4974947d111f3148c
root <root@invento-test>
2013-09-04 13:02:28 +0000

    correct urls and installed apps

--- project_name/urls.py
+++ project_name/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', '{{ project_name }}.views.index'),
+    url(r'^$', 'core.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 )
